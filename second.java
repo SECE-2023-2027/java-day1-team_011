5195d3c66b02e1dd533799fdf7b0e00372e57817
Dharshini-2005 <dharshini.n2023csbs@sece.ac.in>
2026-06-15 18:44:49 +0530

    Added second and third Java programs

---
+++ second.java
@@ -0,0 +1,20 @@
+public class second {
+    public static void main(String[] args) {
+
+        // a. -5 + 8 * 6
+        int a = -5 + 8 * 6;
+        System.out.println(a);
+
+        // b. (55+9) % 9
+        int b = (55 + 9) % 9;
+        System.out.println(b);
+
+        // c. 20 + -3*5 / 8
+        int c = 20 + -3 * 5 / 8;
+        System.out.println(c);
+
+        // d. 5 + 15 / 3 * 2 - 8 % 3
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(d);
+    }
+}

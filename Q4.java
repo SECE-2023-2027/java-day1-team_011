33dbb88db6836e13c080bd349a7f90f0574af5e4
Dhiviya Chandrasekaran <dhiviyachandrasekaran093@gmail.com>
2026-06-15 17:19:22 +0530

    Added Q4

---
+++ Q4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class binary_sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input first binary number: ");
+        String b1 = sc.next();
+
+        System.out.print("Input second binary number: ");
+        String b2 = sc.next();
+
+        int n1 = Integer.parseInt(b1, 2);
+        int n2 = Integer.parseInt(b2, 2);
+
+        int sum = n1 + n2;
+
+        System.out.println("Sum of two binary numbers: " +
+                Integer.toBinaryString(sum));
+    }
+}

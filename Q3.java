8810b88cebef9ed2ff494c858290cbbc14648a9e
Dhiviya Chandrasekaran <dhiviyachandrasekaran093@gmail.com>
2026-06-15 17:18:43 +0530

    Added Q3

---
+++ Q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class area_perimeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        double area = Math.PI * r * r;
+        double perimeter = 2 * Math.PI * r;
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
